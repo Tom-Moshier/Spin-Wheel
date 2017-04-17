4095df1f32f3e005bbf084d6ac029f0e8b7ab2d8
Thomas Moshier <tmoshie1@binghamton.edu>
2017-04-17 16:00:29 -0400

    Created the initial circle and enabled rotation

--- p06-moshier/p06-moshier/GameScene.m
+++ p06-moshier/p06-moshier/GameScene.m
@@ -7,9 +7,19 @@
 //
 
 #import "GameScene.h"
+#import <math.h>
 
 @interface GameScene () <SKPhysicsContactDelegate> {
     SKShapeNode *ball;
+    SKShapeNode *myCircle1;
+    SKShapeNode *myCircle2;
+    SKShapeNode *myCircle3;
+    SKShapeNode *myCircle4;
+    
+    SKShapeNode *myRectangle1;
+    SKShapeNode *myRectangle2;
+    SKShapeNode *myRectangle3;
+    SKShapeNode *myRectangle4;
 }
 
 @end;
@@ -26,11 +36,12 @@
     //creating gravity
     self.physicsWorld.gravity = CGVectorMake(0.0f, -10.0f);
     self.physicsWorld.contactDelegate = self;
+    
+    //making a frame so the ball can't go past
     SKPhysicsBody* borderBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     self.physicsBody = borderBody;
     self.physicsBody.friction = 0.0f;
     
-    //Add a ball with physics
     //Drawing the ball was found from here: http://stackoverflow.com/questions/24078687/draw-smooth-circle-in-ios-sprite-kit
     CGRect circle = CGRectMake(10.0, 10.0, 20.0, 20.0);
     ball = [[SKShapeNode alloc] init];
@@ -39,20 +50,77 @@
     ball.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
     ball.lineWidth = 0;
     
+    //physics for the ball
     ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
-    ball.physicsBody.dynamic = YES;
+    ball.physicsBody.dynamic = NO;
     ball.physicsBody.allowsRotation = NO;
     ball.name = @"Ball";
-    
     [self addChild:ball];
+    
+    [self addCircle];
+    [self rotateCircle];
+    [self addRectangle];
 
 }
 
+- (void)addCircle {
+    // a lot of code has been translated from swift to objective c
+    // following this tutorial: https://www.raywenderlich.com/149034/how-to-make-a-game-like-color-switch-with-spritekit-and-swift
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(0,-200)];
+    [path addLineToPoint:CGPointMake(0, -160)];
+    [path addArcWithCenter:CGPointZero radius:160 startAngle:3.0 * M_PI_2 endAngle:0 clockwise:YES];
+    [path addLineToPoint:CGPointMake(200, 0)];
+    [path addArcWithCenter:CGPointZero radius:200 startAngle:0 endAngle:3.0* M_PI_2 clockwise:NO];
+    
+    myCircle1 = [SKShapeNode shapeNodeWithPath:path.CGPath];
+    myCircle1.strokeColor = [SKColor greenColor];
+    myCircle1.fillColor = [SKColor greenColor];
+    [self addChild:myCircle1];
+    
+    myCircle2 = [SKShapeNode shapeNodeWithPath:path.CGPath];
+    myCircle2.strokeColor = [SKColor redColor];
+    myCircle2.fillColor = [SKColor redColor];
+    myCircle2.zRotation = M_PI_2;
+    [self addChild:myCircle2];
+    
+    myCircle3 = [SKShapeNode shapeNodeWithPath:path.CGPath];
+    myCircle3.strokeColor = [SKColor blueColor];
+    myCircle3.fillColor = [SKColor blueColor];
+    myCircle3.zRotation = M_PI;
+    [self addChild:myCircle3];
+    
+    myCircle4 = [SKShapeNode shapeNodeWithPath:path.CGPath];
+    myCircle4.strokeColor = [SKColor yellowColor];
+    myCircle4.fillColor = [SKColor yellowColor];
+    myCircle4.zRotation = 3*M_PI_2;
+    [self addChild:myCircle4];
+}
+
+- (void)rotateCircle {
+    SKAction *rotation = [SKAction rotateByAngle:2*M_PI duration:20];
+    SKAction *repeat = [SKAction repeatActionForever:rotation];
+    [myCircle1 runAction:repeat];
+    [myCircle2 runAction:repeat];
+    [myCircle3 runAction:repeat];
+    [myCircle4 runAction:repeat];
+}
+
+- (void)addRectangle {
+    
+}
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     //Impluse code was found here: https://digitalbreed.com/2014/02/10/how-to-build-a-game-like-flappy-bird-with-xcode-and-sprite-kit/
-    ball.physicsBody.velocity = CGVectorMake(0, 0);
-    [ball.physicsBody applyImpulse:CGVectorMake(0, 6)];
+    if(ball.physicsBody.dynamic == NO) {
+        ball.physicsBody.dynamic = YES;
+        ball.physicsBody.velocity = CGVectorMake(0, 0);
+        [ball.physicsBody applyImpulse:CGVectorMake(0, 6)];
+    }
+    else {
+        ball.physicsBody.velocity = CGVectorMake(0, 0);
+        [ball.physicsBody applyImpulse:CGVectorMake(0, 6)];
+    }
 }
 
 
